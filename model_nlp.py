4c9297b9e5039bf5c23d8dbed59228ac197db81a
Timothy Liu <tlkh.xms@gmail.com>
2018-08-08 08:41:29 +0000

    Working various models

--- model_nlp.py
+++ model_nlp.py
@@ -7,16 +7,21 @@ from nltk.corpus import stopwords
 
 
 class article_profile_classifier(api_model):
-    """TODO"""
+    """
+    Model to match the writing style of an article to one of few categories
+    Required files:
+      Weights: "profile_weights.h5"
+      Tokenizer: "profile_tokenizer.pickle"
+    """
 
     def __init__(self, debug=True):
         self.name = "Article Profile Classifier"
         self.debug = debug
         self.model = load_model("profile_weights.h5")
         self.model._make_predict_function()
-        self.classes = classes = ["fake", "satire", "bias",
-                                  "conspiracy", "state", "junksci", "hate", "clickbait",
-                                  "unreliable", "political", "reliable"]
+        self.classes = ["fake", "satire", "opinion piece",
+                        "conspiracy", "state", "junk science", "hate speech", "clickbait",
+                        "unreliable", "political", "reliable"]
         with open('profile_tokenizer.pickle', 'rb') as handle:
             self.tokenizer = pickle.load(handle)
         if self.debug:
@@ -45,7 +50,7 @@ class article_profile_classifier(api_model):
         for word in text:
             if word not in stopwords.words("english"):
                 output = output + " " + word
-        return str(output.strip())[1:-3].replace("  ", " ")
+        return output.strip().replace("  ", " ")
 
     def preprocess(self, input_data):
         input_string = self.clean_text(input_data)
@@ -55,17 +60,187 @@ class article_profile_classifier(api_model):
         return processed_input
 
 
+import tensorflow as tf
+import tensorflow_hub as hub
+
+from keras import regularizers, initializers, optimizers, callbacks
+from keras.utils.np_utils import to_categorical
+from keras.layers import *
+from keras.models import Model
+from keras import backend as K
+
+
+class subjectivity_classifier(api_model):
+    """
+    Model to detect subjective language in an article
+    Required files:
+      Weights: "subjective_weights.h5"
+    """
+
+    def __init__(self, debug=True):
+        self.name = "Subjectivity Classifier"
+        self.debug = debug
+        self.model = self.build_model()
+        self.model.load_weights("subjective_weights.h5")
+        self.model._make_predict_function()
+        self.MAX_SEQUENCE_LENGTH = 35
+        self.classes = ["subjective", "objective"]
+        if self.debug:
+            if self.run_self_test():
+                print(" * [i] Model: " + self.name +
+                      " has loaded successfully")
+            else:
+                print(" * [!] An error has occured in self test!!")
+
+    def build_model(self):
+        """Text Classification with pre-trainedELMo embeddings from TF Hub"""
+        sess = tf.Session()
+        K.set_session(sess)
+
+        elmo_model = hub.Module(
+            "https://tfhub.dev/google/elmo/2", trainable=True)
+        sess.run(tf.global_variables_initializer())
+        sess.run(tf.tables_initializer())
+
+        sequence_input = Input(shape=(1,), dtype=tf.string)
+        embedded_sequences = Lambda(lambda x: elmo_model(tf.squeeze(tf.cast(
+            x, tf.string)), signature="default", as_dict=True)["default"], output_shape=(1024,))(sequence_input)
+        embedded_sequences = Reshape((1024, 1,))(embedded_sequences)
+        l_drop = Dropout(0.4)(embedded_sequences)
+        l_flat = Flatten()(l_drop)
+        l_dense = Dense(32, activation='relu')(l_flat)
+        preds = Dense(2, activation='softmax')(l_dense)
+        model = Model(sequence_input, preds)
+
+        model.compile(loss='binary_crossentropy',
+                      optimizer="adam", metrics=['acc'])
+
+        return model
+
+    def run_self_test(self):
+        # leave in a simple test to see if the model runs
+        # also to take a quick benchmark to test performance
+        test = ["Hello this is a random statement",
+                "Hello this is another statement"]
+        test = np.array(test, dtype=object)[:, np.newaxis]
+        preds_list = self.model.predict(test)
+        return True
+
+    def predict(self, input_data):
+        batch = self.preprocess(input_data)
+        preds_list = self.model.predict(batch)
+        output = np.asarray([0, 0])
+        for pred in preds_list:
+            output = output + pred
+        return output.tolist()
+
+    def preprocess(self, text):
+        text = re.sub(r'[^\w\s]', ' ', text.lower()).replace(
+            "\n", " ").replace("  ", " ")
+        text = "".join([c for c in text if (c.isalpha() or c == " ")])
+        text = text.split(" ")
+        batch = []
+        count = 0
+        seq = ""
+        for i, word in enumerate(text):
+            if count > self.MAX_SEQUENCE_LENGTH-1:
+                count = 0
+                batch.append(seq)
+                seq = ""
+            elif (word.isalpha() and word != " "):
+                seq = seq + word + " "
+                count += 1
+            elif i == len(text)-1:
+                if len(seq) > 5:
+                    batch.append(seq)
+        return np.array(batch, dtype=object)[:, np.newaxis]
+
+
+class toxic_classifier(api_model):
+    """
+    Model to detect hateful language in an article
+    Required files:
+      Weights: "toxic_weights.h5"
+      Tokenizer: "toxic_tokenizer.pickle"
+    """
+
+    def __init__(self, debug=True):
+        self.name = "Toxicity Classifier"
+        self.debug = debug
+        self.model = load_model("toxic_weights.h5")
+        self.model._make_predict_function()
+        self.MAX_SEQUENCE_LENGTH = 128
+        self.classes = ["toxic", "severe_toxic",
+                        "obscene", "threat", "insult", "identity_hate", "none"]
+        with open('toxic_tokenizer.pickle', 'rb') as handle:
+            self.tokenizer = pickle.load(handle)
+        if self.debug:
+            if self.run_self_test():
+                print(" * [i] Model: " + self.name +
+                      " has loaded successfully")
+            else:
+                print(" * [!] An error has occured in self test!!")
+
+    def run_self_test(self):
+        # leave in a simple test to see if the model runs
+        # also to take a quick benchmark to test performance
+        return True
+
+    def predict(self, input_data):
+        input_sequence = self.preprocess(input_data)
+        preds_list = self.model.predict(input_sequence)
+        output = []
+        for pred in preds_list:
+            for i, class_pred in enumerate(pred):
+                if class_pred > 0.6:
+                    if self.classes[i] not in output:
+                        output.append(self.classes[i])
+
+        if len(output) < 1:
+            return ["none"]
+        else:
+            return output
+
+    def clean_text(self, text):
+        text = re.sub(r'[^\w\s]', ' ', text.lower()).replace(
+            "\n", " ").replace("  ", " ")
+        text = "".join([c for c in text if (c.isalpha() or c == " ")])
+        text = text.split(" ")
+        batch = []
+        count = 0
+        seq = ""
+        for i, word in enumerate(text):
+            if count > 110:
+                count = 0
+                batch.append(seq)
+                seq = ""
+            elif (word.isalpha() and word != " " and word not in stopwords.words("english")):
+                seq = seq + word + " "
+                count += 1
+            elif i == len(text)-1:
+                if len(seq) > 5:
+                    batch.append(seq)
+        return batch
+
+    def preprocess(self, input_data):
+        input_string = self.clean_text(input_data)
+        input_token = self.tokenizer.texts_to_sequences(input_string)
+        processed_input = pad_sequences(
+            input_token, padding='pre', maxlen=(self.MAX_SEQUENCE_LENGTH-3))
+        processed_input = pad_sequences(
+            processed_input, padding='post', maxlen=(self.MAX_SEQUENCE_LENGTH))
+        return processed_input
+
+
 class clickbait_detector(api_model):
     """
     Model to detect if the article title is clickbait
     Required files:
       Weights: "clickbait_weights.h5"
       Tokenizer: "clickbait_tokenizer.pickle"
-    Usage:
-      clickbait.predict()
     """
 
-    def __init__(self, debug=True):
+    def __init__(self, debug=False):
         print(" * [i] Loading model...")
         self.name = "Clickbait Detector"
         self.model = load_model("clickbait_weights.h5")
--- server.py
+++ server.py
@@ -8,24 +8,30 @@ from io import BytesIO
 
 from newspaper import Article
 
+from threading import Thread
+
 app = flask.Flask(__name__)
 
 from model_nlp import *
 
 model_clickbait = None
+model_toxic = None
 model_profile = None
+model_subj = None
 
 urllib3.disable_warnings()
+np.warnings.filterwarnings('ignore')
 
 
 def load_ML():
     print(" * [i] Building Keras models")
-    global model_clickbait, model_profile
+    global model_clickbait, model_profile, model_toxic, model_subj
 
-    # clickbait title detector
+    model_subj = subjectivity_classifier()
     model_clickbait = clickbait_detector()
+    model_toxic = toxic_classifier()
 
-    # article profile classifier
+    # article profile/type classifier
     model_profile = article_profile_classifier()
 
 
@@ -33,50 +39,69 @@ def load_ML():
 def predict():
         # initialize the data dictionary that will be returned from the
         # view
-    global model_clickbait, model_profile
+    global model_clickbait, model_profile, model_toxic, model_subj
 
     data = {"success": False}
 
     # get the respective args from the post request
     if flask.request.method == "POST":
         start_time = time.time()
-        try:
-            # retrieve parameters from arguments
-            article_url = flask.request.args.get("article_url")
+        # retrieve parameters from arguments
+        article_url = flask.request.args.get("article_url")
 
-            article = Article(article_url, fetch_images=False)
-            article.download()
-            article.parse()
+        article = Article(article_url, fetch_images=False)
+        article.download()
+        article.parse()
 
-            article_title = article.title
-            article_text = article.text
-            image_list = article.images
+        article_title = article.title
+        article_text = article.text
+        image_list = article.images
 
-            if article_title is not None:
-                article_title = article_title.replace("%20", " ")
-                print(" * [i] Incoming article title:", article_title)
-                pred_clickbait = model_clickbait.predict(article_title)
-                data["clickbait"] = pred_clickbait
-                data["article_title"] = article_title
+        threads = []
 
-            if article_text is not None:
-                pred_profile = model_profile.predict(article_text)
-                data["article_profile"] = pred_profile
+        def pred_clickbait(input_):
+            data["clickbait"] = model_clickbait.predict(input_)
 
-            if image_list is not None:
-                results = []
-                # TODO
+        def pred_toxic(input_):
+            data["article_toxic"] = model_toxic.predict(input_)
 
-                data["hoax_image_search"] = results
+        def pred_profile(input_):
+            data["article_profile"] = model_profile.predict(input_)
 
-            data["success"] = True
+        def pred_subj(input_):
+            data["article_subjectivity"] = model_subj.predict(input_)
 
-        except Exception as e:
-            data["prediction_error"] = str(e)
-            data["success"] = False
-            print(" * [!]", e)
+        if article_title is not None:
+            article_title = article_title.replace("%20", " ")
+            print(" * [i] Incoming article title:", article_title)
+            data["article_title"] = article_title
 
-        print("Request took", int(time.time()-start_time), "seconds")
+            t = Thread(target=pred_clickbait, args=([article_title]))
+            threads.append(t)
+            t.start()
+
+        if article_text is not None:
+            t = Thread(target=pred_toxic, args=([article_text]))
+            threads.append(t)
+            t.start()
+
+            t = Thread(target=pred_profile, args=([article_text]))
+            threads.append(t)
+            t.start()
+
+            t = Thread(target=pred_subj, args=([article_text]))
+            threads.append(t)
+            t.start()
+            
+        if image_list is not None:
+            results = []
+            # TODO
+            data["hoax_image_search"] = results
+
+        [t.join() for t in threads]
+        data["success"] = True
+
+        print("Request took", round(time.time()-start_time, 3), "seconds")
 
     # return the data dictionary as a JSON response
     return flask.jsonify(data)
